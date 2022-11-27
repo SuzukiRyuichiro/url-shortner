08db7ad1fe8a2c5d9071e13c4cff8238c2811d69
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-27 17:33:18 +0100

    running

--- api/src/model.js
+++ api/src/model.js
@@ -7,6 +7,7 @@ const client = createClient({
     host: process.env.REDIS_HOST || 'redis',
     port: process.env.REDIS_PORT || 6379,
   },
+  password: process.env.REDIS_PASSWORD,
 })
 
 client.on('connect', () => {
